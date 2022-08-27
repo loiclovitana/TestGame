df2147d735494031b10da62700a50939e54be4d3
loiclovitana <loiclovitana@gmail.com>
2022-08-27 16:28:10 +0200

    add project

---
+++ desktop/src/ch/jelo/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ch.jelo.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ch.jelo.game.TestGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("test_game");
+		new Lwjgl3Application(new TestGame(), config);
+	}
+}
